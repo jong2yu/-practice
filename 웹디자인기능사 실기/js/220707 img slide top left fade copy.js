e7ca4137d0d7dd8d1395facdf757045a3ca26726
jong2yu <97148584+jong2yu@users.noreply.github.com>
2022-07-07 17:13:41 +0900

    Add files via upload

--- 웹디자인기능사 실기/js/220707 img slide top left fade copy.js
+++ 웹디자인기능사 실기/js/220707 img slide top left fade copy.js
@@ -5,20 +5,20 @@ $(document).ready(function(){
       $(".slide.top .wrap li:first").appendTo(".slide.top .wrap");
       $(".slide.top .wrap").css("margin-top","0px");
     });
-  }, 3000);
+  },3000);
 
   setInterval(function(){
     $(".slide.left .wrap").animate({marginLeft:"-=640px"}, function(){
       $(".slide.left .wrap li:first").appendTo(".slide.left .wrap");
       $(".slide.left .wrap").css("margin-left","0px");
     });
-  }, 3000);
+  },3000);
 
-  $(".slide.fade > div:gt(0)").hide();
+  $(".slide.fade div:gt(0)").hide();
   setInterval(function(){
-    $('.slide.fade > div:first').fadeOut()
-    .next('.slide.fade > div').fadeIn()
-    .end().appendTo('.slide.fade');
-  }, 3000);
+    $(".slide.fade div:first").fadeOut(1000)
+    .next(".slide.fade div").fadeIn(1000)
+    .end().appendTo(".slide.fade")
+  }, 3000)
 
 });
