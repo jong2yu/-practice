9ddee4ec149602a6dacc6162855c64663f550d1e
jong2yu <97148584+jong2yu@users.noreply.github.com>
2022-07-17 13:15:16 +0900

    Add files via upload

---
+++ 웹디자인기능사 실기/js/220717 full.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  $(".menu .sub").hide();
+  $(".menu .main").mouseover(function(){
+    $(".menu .sub").stop().slideDown(300)
+  }).mouseout(function(){
+    $(".menu .sub").stop().slideUp(300)
+  });
+
+  $(".slide a:gt(0)").hide();
+  setInterval(function(){
+    $(".slide a:first").fadeOut(1000)
+    .next(".slide a").fadeIn(1000)
+    .end().appendTo(".slide")
+  },3000);
+
+  $(".tab .title").click(function(){
+    $(this).parent().addClass("active").siblings().removeClass("active")
+  });
+
+  $("#modal").hide();
+  $(".notice a:first").click(function(){
+    $("#modal").show();
+  });
+  $("#modal button").click(function(){
+    $("#modal").hide();
+  });
+
+});
